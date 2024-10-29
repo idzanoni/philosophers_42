067f81b05b48bc624a38d90d2149324353d1c7b3
Izabel Zanoni <izanoni@c1r5p1.42sp.org.br>
2024-10-29 17:09:59 -0300

    organizaçao de arquivos

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: izanoni <izanoni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 18:34:04 by izanoni           #+#    #+#             */
-/*   Updated: 2024/10/28 20:37:12 by izanoni          ###   ########.fr       */
+/*   Updated: 2024/10/29 16:30:48 by izanoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	init_routine(t_philo *thinker)
 	int	i;
 
 	i = 0;
-	pthread_mutex_init(thinker->ph_data->mutex_alive);
-	pthread_mutex_init(thinker->ph_data->mutex_print);
+	pthread_mutex_init(&thinker->ph_data->mutex_alive, NULL);
+	pthread_mutex_init(&thinker->ph_data->mutex_print, NULL);
 	while (i < thinker->ph_data->philo_range)
 	{
 		pthread_create(&thinker[i].thread, NULL, routine, &thinker[i]);
@@ -36,7 +36,7 @@ void	init_routine(t_philo *thinker)
 void *routine(void *arg)
 {
 	t_philo *thinker;
-	
+
 	thinker = arg;
 	printf ("eating\n");
 	printf ("sleeping\n");
